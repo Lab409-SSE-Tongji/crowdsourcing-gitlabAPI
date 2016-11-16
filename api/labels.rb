cdf8befe8d2c7a1f670e2c9ed71c045d8ec018f6
yuhaopp <yuhaodovepp@outlook.com>
2016-11-16 20:25:11 +0800

    使用时，将原代码的lib/api文件夹替换为这一仓库中的api文件夹。
    关于project->files这一功能调用API说明：
    0.目前没有找到很好的方法来获取每个文件的commit时间及描述。只能通过分别调用两个API来分别获取目录及相应的message。具体如下：
    1.使用GET "/projects/:id/repository/tree"方法获得代码文件目录。
    2.在上一API中有commit_id字段，再可使用GET "/projects/:id/repository/commits/:sha"获得相应的时间以及commit message。其中，sha参数即为commit_id。

---
+++ api/labels.rb
@@ -0,0 +1,85 @@
+module API
+  # Labels API
+  class Labels < Grape::API
+    before { authenticate! }
+
+    params do
+      requires :id, type: String, desc: 'The ID of a project'
+    end
+    resource :projects do
+      desc 'Get all labels of the project' do
+        success Entities::Label
+      end
+      get ':id/labels' do
+        present available_labels, with: Entities::Label, current_user: current_user
+      end
+
+      desc 'Create a new label' do
+        success Entities::Label
+      end
+      params do
+        requires :name, type: String, desc: 'The name of the label to be created'
+        requires :color, type: String, desc: "The color of the label given in 6-digit hex notation with leading '#' sign (e.g. #FFAABB)"
+        optional :description, type: String, desc: 'The description of label to be created'
+      end
+      post ':id/labels' do
+        authorize! :admin_label, user_project
+
+        label = available_labels.find_by(title: params[:name])
+        conflict!('Label already exists') if label
+
+        label = user_project.labels.create(declared(params, include_parent_namespaces: false).to_h)
+
+        if label.valid?
+          present label, with: Entities::Label, current_user: current_user
+        else
+          render_validation_error!(label)
+        end
+      end
+
+      desc 'Delete an existing label' do
+        success Entities::Label
+      end
+      params do
+        requires :name, type: String, desc: 'The name of the label to be deleted'
+      end
+      delete ':id/labels' do
+        authorize! :admin_label, user_project
+
+        label = user_project.labels.find_by(title: params[:name])
+        not_found!('Label') unless label
+
+        present label.destroy, with: Entities::Label, current_user: current_user
+      end
+
+      desc 'Update an existing label. At least one optional parameter is required.' do
+        success Entities::Label
+      end
+      params do
+        requires :name,  type: String, desc: 'The name of the label to be updated'
+        optional :new_name, type: String, desc: 'The new name of the label'
+        optional :color, type: String, desc: "The new color of the label given in 6-digit hex notation with leading '#' sign (e.g. #FFAABB)"
+        optional :description, type: String, desc: 'The new description of label'
+        at_least_one_of :new_name, :color, :description
+      end
+      put ':id/labels' do
+        authorize! :admin_label, user_project
+
+        label = user_project.labels.find_by(title: params[:name])
+        not_found!('Label not found') unless label
+
+        update_params = declared(params,
+                                 include_parent_namespaces: false,
+                                 include_missing: false).to_h
+        # Rename new name to the actual label attribute name
+        update_params['name'] = update_params.delete('new_name') if update_params.key?('new_name')
+
+        if label.update(update_params)
+          present label, with: Entities::Label, current_user: current_user
+        else
+          render_validation_error!(label)
+        end
+      end
+    end
+  end
+end
